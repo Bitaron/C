aa02abca893b8f5a88e1df52eb3f10fa97829333
rony <rony@commlinkinfotech.com>
2018-04-11 15:47:04 +0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/initial_setUP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/initial_setUP.dir/main.c.o"
+  "CMakeFiles/initial_setUP.dir/add.c.o"
+  "initial_setUP.pdb"
+  "initial_setUP.exe"
+  "libinitial_setUP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/initial_setUP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/initial_setUP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for initial_setUP.
+# This may be replaced when dependencies are built.
